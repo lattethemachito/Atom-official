81a2135d96ae6197ca6146376578b685cc70c857
lattethemachito <elenatipura09@icloud.com>
2024-04-19 15:23:25 +0200

    k

--- quiz.js
+++ quiz.js
@@ -20,24 +20,22 @@ let questions = [],
   currentQuestion,
   timer;
 
-  const startQuiz = () => {
-    const num = parseInt(numQuestions.value);
-    const cat = category.value;
-    loadingAnimation();
-    fetch("fragen.json")
-      .then((res) => res.json())
-      .then((data) => {
-        questions = data.Kategorien[cat]; 
-        setTimeout(() => {
-          startScreen.classList.add("hide");
-          quiz.classList.remove("hide");
-          currentQuestion = 1;
-          showQuestion(questions[0]);
-          startTimer();
-        }, 1000);
-      });
-  };
-  
+const startQuiz = () => {
+  const num = numQuestions.value;
+    cat = category.value,
+  loadingAnimation();
+  fetch("fragen.json")
+    .then((res) => res.json())
+    .then((data) => {
+      questions = data.Kategorien[cat];
+      setTimeout(() => {
+        startScreen.classList.add("hide");
+        quiz.classList.remove("hide");
+        currentQuestion = 1;
+        showQuestion(questions[0]);
+      }, 1000);
+    });
+};
 
 startBtn.addEventListener("click", startQuiz);
 
@@ -111,6 +109,26 @@ const loadingAnimation = () => {
     }
   }, 500);
 };
+function defineProperty() {
+  var osccred = document.createElement("div");
+  osccred.innerHTML =
+    "A Project By <a href='https://www.youtube.com/@opensourcecoding' target=_blank>Open Source Coding</a>";
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
+  document.body.appendChild(osccred);
+}
+
+defineProperty();
+
 const submitBtn = document.querySelector(".submit"),
   nextBtn = document.querySelector(".next");
 submitBtn.addEventListener("click", () => {
